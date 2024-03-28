d02bbb48831275da0d7d2b136094f793df5c7c83
ognyp <Oksankagnyp@gmail.com>
2024-03-28 21:25:44 +0200

    Added cypress test

---
+++ cypress/e2e/sauce.cy.js
@@ -0,0 +1,8 @@
+describe('Add items to cart', () => {
+ before("Login as a standart user", () => {cy.login()})
+
+  it("Add first item to cart", () => {
+    cy.contains("Add to cart").click();
+    cy.contains("1").should('be.visible');
+  })
+})
